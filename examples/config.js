056404f8695c75f29fa31ca859a58e973ec91fbf
Lumir Mrkva <lumir.mrkva@topmonks.com>
2015-11-26 13:55:50 +0100

    another config example (random)

---
+++ examples/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    interval: 1,
+    pages: [
+        {
+            name: 'random',
+            url: 'https://www.random.org/integers/?num=1&min=1&max=2&col=1&base=10&format=plain'
+        }
+    ],
+    notifications: {}
+};
